99e6fbaf564c6785dedfbd12d2e89b3f14af29d0
emanuele <emanu.alterio@gmail.com>
2025-07-29 17:40:02 +0200

    prova bootstrap

---
+++ src/frontend/src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../elements/Navbar/navbar';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <h1 className="text-center mb-4">Benvenuto su StreetCats!</h1>
+        <p className="lead text-center">
+          Qui puoi segnalare avvistamenti di gatti randagi e scoprire storie e foto di tanti amici felini.
+        </p>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-primary btn-lg">Inserisci un nuovo gatto</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/frontend/src/elements/Navbar/navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import petIcon from '../../assets/petIcon.svg';
+import './navbar.scss';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3" >
+      <div className="container-fluid">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img
+            src={petIcon}
+            alt="StreetCats"
+            width="30"
+            height="30"
+            className="d-inline-block align-text-top me-2"
+          />
+          StreetCats
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/add-cat">Inserisci Gatto</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
