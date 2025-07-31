8c236c483f928783fa23fa304530ed4ff5e57195
emanuele <emanu.alterio@gmail.com>
2025-07-31 15:53:47 +0200

    -Creazione del db. -Inizializzaione connessione db.

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,7 @@
 // index.js
 const express = require('express');
 const cors = require('cors');
+const pool = require('./config/db');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,17 @@ const PORT = process.env.PORT || 3001;
 // Middleware base
 app.use(cors());
 app.use(express.json());
+
+// Test DB
+app.get('/ping', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error('Errore nella connessione al DB:', err);
+    res.status(500).json({ error: 'Errore nel ping del database' });
+  }
+});
 
 // Test route
 app.get('/', (req, res) => {
